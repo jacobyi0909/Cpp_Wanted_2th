cfcfd361306be3835b2d4dba8cd7ee285a78f027
jacobyi <jacobyi0909@gmail.com>
2025-08-06 14:58:36 +0900

    20250806

---
+++ Class2/Class2.cpp
@@ -0,0 +1,42 @@
+﻿/*
+
+#include <iostream>
+#include <vector>
+
+class AActor {
+public:
+	void BeginPlay()
+	{
+	}
+
+	void Tick()
+	{
+	}
+};
+
+class APawn : public AActor {
+
+};
+int main()
+{
+	std::vector<AActor*> list;
+	float currentTime = 현재시간;
+	while (true)
+	{
+		// 화면 처리
+		// 마우스 오른쪽 버튼을 누르면 AActor를 생성하게되면 
+		if ()
+		{
+			auto* newActor = new AActor();
+			list.push_back(newActor);
+			newActor->BeginPlay();
+		}
+		float deltaSecond = 현재시간 - currentTime;
+		currentTime = 현재시간;
+
+		for (auto* actor : list)
+		{
+			actor->Tick(deltaSecond);
+		}
+	}
+}*/
--- ClassExample/ClassExample.cpp
+++ ClassExample/ClassExample.cpp
@@ -1,26 +1,182 @@
 ﻿
 
 #include <iostream>
+
 // class 사용자 정의 자료형
 class Animal
 {
 public:
+
+	Animal() {
+
+	}
+
+	Animal(int newAge, int newGold) : age(newAge), gold(newGold)
+	{
+	}
+	int gold;
+
+	// 순수 가상함수
+	virtual void Walk() = 0;
+
 	int age;
 
-	void Print()
+	virtual void Print()
 	{
 		std::cout << age << std::endl;
 	}
 
+	// 가상함수
+	virtual void Attack()
+	{
+		std::cout << "Animal Attack" << std::endl;
+	}
+};
+
+// 상속
+class Cat /*final*/ : public Animal {
+
+public:
+
+	//void Print() final
+	//{
+	//}
+	Cat()
+	{
+		std::cout << "Cat 생성자" << std::endl;
+	}
+
+	Cat(int newAge, int newGold) : Animal(newAge, newGold)
+	{
+	}
+	// 복사 생성자
+	Cat(const Cat& other)
+	{
+		std::cout << "Cat 복사 생성자" << std::endl;
+		age = other.age;
+	}
+
+	virtual void Attack() override
+	{
+		std::cout << "냥냥펀치" << std::endl;
+	}
+	virtual void Walk() override
+	{
+
+	}
+
+};
+
+class Tiger : public Cat
+{
+public:
+	virtual void Print() override
+	{
+
+	}
 };
 
 
-
-int main()
+void main()
 {
-	Animal cat;
+	Cat cat;
+	cat.age = 1;
 
-	cat.age = 10;
+	Cat cat2(cat);
+	//Cat cat2 = cat;
+	cat2.age = cat.age;
+
+
+
+	
+
+	//cat.Attack();
+
+	// 업 캐스팅은 된다.
+	Animal* pAni = &cat;
+	pAni->Attack();
+
+	// 다운캐스팅은 안됨.
+	//Animal ani;
+	//Cat* pCat2 = &ani;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// 공용체
+union aaa {
+	int a;
+	char b[4];
+};
+
+// 구조체 struct
+struct MyStruct {
+public:
+private:
+};
+
+
+// Call by pointer
+int Add(int* a, int* b)
+{
+	*a = 300;
+	return *a + *b;
+}
+// Call by value
+int Add(int a, int b)
+{
+	a = 300;
+	return a + b;
+}
+// Call by reference
+int AddRef(int& a, int& b)
+{
+	a = 300;
+	return a + b;
+}
+struct HitResult {
+	int name;
+};
+
+
+void main2()
+{
+	int i = 10;
+	int j = 230;
+	int ret = AddRef(i, j);
+
+
+	// 참조 Reference
+
+	/*int KK = 0;
+	int& RR = KK;
+
+	std::cout << &KK << std::endl;
+	std::cout << &RR << std::endl;*/
+	//int k;	// 변수 선언
+	//k = 10;	// 변수에 값을 대입
+	//int r = 10;// 변수r을 10으로 초기화
+
+
+
+
+
+
+	//int a(10);
+
+	//Animal cat(10);
+	//Animal* pCat = &cat;
+
+	//std::cout << cat.age << std::endl;
 
 	//Animal* dog = new Animal;
 
@@ -30,4 +186,24 @@ int main()
 	//{
 	//	delete dog;
 	//}
+
+
+
+	//Test();
+
+	//aaa aa;
+	//aa.a  = 1;
+	//for (int i=0 ; i<4 ; i++)
+	//{
+	//	std::cout << (int)aa.b[i] << ' ';
+	//}
+}
+
+void Test()
+{
+	int* a = new int();
+	int* b = a;
+
+	delete a;
+	// b는 댕글링 포인터
 }
